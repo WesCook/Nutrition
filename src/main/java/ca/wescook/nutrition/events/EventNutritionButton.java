2b8b86a3da2eabe0f9f03fab440587c0a469c347
Wes Cook <im@wescook.ca>
2017-05-16 11:53:10 -0700

    Simplifying GUI button placement with an AT

--- src/main/java/ca/wescook/nutrition/events/EventNutritionButton.java
+++ src/main/java/ca/wescook/nutrition/events/EventNutritionButton.java
@@ -10,8 +10,6 @@ import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -30,23 +28,10 @@ public class EventNutritionButton {
 		if (!(gui instanceof GuiInventory))
 			return;
 
-		// Check if any potion effects are visible
-		boolean visiblePotionEffect = false;
-		for (PotionEffect potioneffect : Minecraft.getMinecraft().thePlayer.getActivePotionEffects()) {
-			Potion potion = potioneffect.getPotion();
-			if (potion.shouldRender(potioneffect)) {
-				visiblePotionEffect = true;
-				break;
-			}
-		}
-
-		// Button position (calculate offset for potion effects)
-		int xPosition;
-		int yPosition = (gui.height / 2) - 22;
-		if (!visiblePotionEffect)
-			xPosition = (gui.width / 2) + 10; // Normal offset
-		else
-			xPosition = 160 + ((gui.width - 200) / 2) + 10; // Potion offset
+		// TODO: 1.11 offers GuiContainer#getGuiLeft().  Remove access transformers in 1.11 port.
+		// Button position
+		int xPosition = ((GuiInventory) gui).guiLeft + 97;
+		int yPosition = ((GuiInventory) gui).guiTop + 61;
 
 		// Create button
 		event.getButtonList().add(this.buttonNutrition = new GuiButtonIcon(NUTRITION_ID, xPosition, yPosition, 18, 17, new ItemStack(Items.CARROT)));
