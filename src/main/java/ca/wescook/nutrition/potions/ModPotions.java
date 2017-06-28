b71f306c6c47bba40aee807459e2c0aa12460864
Wes Cook <im@wescook.ca>
2017-06-28 15:19:16 -0700

    Adding nourished effect; revamping malnourished to only affect health

--- src/main/java/ca/wescook/nutrition/potions/ModPotions.java
+++ src/main/java/ca/wescook/nutrition/potions/ModPotions.java
@@ -10,11 +10,13 @@ import java.util.UUID;
 public class ModPotions {
 	public static PotionToughness toughness;
 	public static PotionMalnourished malnourished;
+	public static PotionNourished nourished;
+
 	static final UUID TOUGHNESS_HEALTH = UUID.fromString("d80b5ec3-8cf9-4b74-bc0d-6f3ef7b48b2e");
 	static final UUID TOUGHNESS_ARMOR = UUID.fromString("f42431e4-8efc-44d2-8249-fea2a2cb418e");
 	static final UUID TOUGHNESS_ATTACK_SPEED = UUID.fromString("10d42c27-f160-4909-a523-9b2553e14eac");
+	static final UUID NOURISHMENT_HEALTH = UUID.fromString("bdafe0c7-5881-4505-802e-e18f6c419554");
 	static final UUID MALNOURISHMENT_HEALTH = UUID.fromString("ea9cebf7-7c7a-4a89-a04f-221dab8ffdf7");
-	static final UUID MALNOURISHMENT_ATTACK_SPEED = UUID.fromString("a5db5ff0-3c49-4804-888b-6932430298d2");
 
 	public static void registerPotions() {
 		// Toughness
@@ -27,12 +29,18 @@ public class ModPotions {
 		toughness.registerPotionAttributeModifier(SharedMonsterAttributes.ATTACK_SPEED, TOUGHNESS_ATTACK_SPEED.toString(), 0D, 0);
 		GameRegistry.register(toughness);
 
+		// Nourished
+		nourished = new PotionNourished(true, new ResourceLocation("nutrition", "textures/potions/nourished.png"));
+		nourished.setPotionName("Nourished");
+		nourished.setRegistryName("nourished");
+		nourished.registerPotionAttributeModifier(SharedMonsterAttributes.MAX_HEALTH, NOURISHMENT_HEALTH.toString(), 0D, 0);
+		GameRegistry.register(nourished);
+
 		// Malnourished
 		malnourished = new PotionMalnourished(true, new ResourceLocation("nutrition", "textures/potions/malnourished.png"));
 		malnourished.setPotionName("Malnourished");
 		malnourished.setRegistryName("malnourished");
 		malnourished.registerPotionAttributeModifier(SharedMonsterAttributes.MAX_HEALTH, MALNOURISHMENT_HEALTH.toString(), 0D, 0);
-		malnourished.registerPotionAttributeModifier(SharedMonsterAttributes.ATTACK_SPEED, MALNOURISHMENT_ATTACK_SPEED.toString(), 0D, 0);
 		GameRegistry.register(malnourished);
 	}
 }
