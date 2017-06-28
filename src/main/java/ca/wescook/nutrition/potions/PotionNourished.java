75846f1fe8e36943b3864a16f5a099b0e84b626e
Wes Cook <im@wescook.ca>
2017-06-28 15:19:16 -0700

    Adding nourished effect; revamping malnourished to only affect health

--- src/main/java/ca/wescook/nutrition/potions/PotionNourished.java
+++ src/main/java/ca/wescook/nutrition/potions/PotionNourished.java
@@ -3,18 +3,18 @@ package ca.wescook.nutrition.potions;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.util.ResourceLocation;
 
-public class PotionMalnourished extends PotionCustom {
+public class PotionNourished extends PotionCustom {
 
-	PotionMalnourished(boolean visibility, ResourceLocation icon) {
+	PotionNourished(boolean visibility, ResourceLocation icon) {
 		super(visibility, icon);
 	}
 
 	// Multiply effect based on amplifier
 	@Override
 	public double getAttributeModifierAmount(int amplifier, AttributeModifier modifier) {
-		// Reduce health
-		if (modifier.getID().equals(ModPotions.MALNOURISHMENT_HEALTH))
-			return 0 - (amplifier + 1) * 2D; // 2 = one heart
+		// Increase health
+		if (modifier.getID().equals(ModPotions.NOURISHMENT_HEALTH))
+			return (amplifier + 1) * 2D; // 2 = one heart
 
 		return 0D;
 	}
